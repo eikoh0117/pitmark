bad78c8f06d0532142ce74186930a171a59ba36c
Hidemitsu Aoki <olympos0117@gmail.com>
2020-05-13 08:46:44 +0900

    Modify location of firabaseConfig from index.js to main.js

--- app/pitmark/src/main.js
+++ app/pitmark/src/main.js
@@ -1,10 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import firebase from "firebase";
 Vue.config.productionTip = false;
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAhHVzs0X2jlLavbnjQcFOYrrg2fyiIDxI",
+  authDomain: "pitmark-d1c0f.firebaseapp.com",
+  databaseURL: "https://pitmark-d1c0f.firebaseio.com",
+  projectId: "pitmark-d1c0f",
+  storageBucket: "pitmark-d1c0f.appspot.com",
+  messagingSenderId: "331188842084",
+  appId: "1:331188842084:web:e5c87067a9945e18f4f971",
+  measurementId: "G-BBKFVM43WY",
+};
+
+firebase.initializeApp(firebaseConfig);
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- app/pitmark/src/router/index.js
+++ app/pitmark/src/router/index.js
@@ -3,18 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import SignUp from "../views/SignUp.vue";
 import SignIn from "../views/SignIn.vue";
-import firebase from "firebase";
-const firebaseConfig = {
-  apiKey: "AIzaSyAhHVzs0X2jlLavbnjQcFOYrrg2fyiIDxI",
-  authDomain: "pitmark-d1c0f.firebaseapp.com",
-  databaseURL: "https://pitmark-d1c0f.firebaseio.com",
-  projectId: "pitmark-d1c0f",
-  storageBucket: "pitmark-d1c0f.appspot.com",
-  messagingSenderId: "331188842084",
-  appId: "1:331188842084:web:e5c87067a9945e18f4f971",
-  measurementId: "G-BBKFVM43WY",
-};
-firebase.initializeApp(firebaseConfig);
+
 Vue.use(VueRouter);
 
 const routes = [
