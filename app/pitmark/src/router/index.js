bbbfa592834b1702cc8f6223b9c98f06e21bd724
Hidemitsu Aoki <olympos0117@gmail.com>
2020-05-12 21:33:02 +0900

    Make it possible for user to signin

--- app/pitmark/src/router/index.js
+++ app/pitmark/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import SignUp from "../views/SignUp.vue";
+import SignIn from "../views/SignIn.vue";
 import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyAhHVzs0X2jlLavbnjQcFOYrrg2fyiIDxI",
@@ -26,6 +27,11 @@ const routes = [
     path: "/sign_up",
     name: "sign_up",
     component: SignUp,
+  },
+  {
+    path: "/sign_in",
+    name: "sign_in",
+    component: SignIn,
   },
 ];
 
