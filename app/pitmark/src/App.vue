4ccd41abbc3074101fd21bd52247d2f89ee7ed0e
Hidemitsu Aoki <olympos0117@gmail.com>
2020-05-12 22:11:40 +0900

    Make it possible for user to signout

--- app/pitmark/src/App.vue
+++ app/pitmark/src/App.vue
@@ -18,7 +18,41 @@
           </a>
         </div>
         <div id="navbar-links" class="navbar-menu">
-          <div class="navbar-end"></div>
+          <div v-if="user" class="navbar-end">
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                <span class="icon">
+                  <i class="fas fa-user"></i>
+                </span>
+              </a>
+              <div class="navbar-dropdown is-right">
+                <a @click="signOut" class="navbar-item">
+                  <span class="icon">
+                    <i class="fas fa-sign-out-alt"></i>
+                  </span>
+                  <span>サインアウト</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div v-else class="navbar-end">
+            <div class="navbar-item">
+              <router-link :to="{ name: 'sign_in' }" class="button is-text">
+                <span class="icon">
+                  <i class="fas fa-sign-in-alt"></i>
+                </span>
+                <span>サインイン</span>
+              </router-link>
+            </div>
+            <div class="navbar-item">
+              <router-link :to="{ name: 'sign_up' }" class="button is-text">
+                <span class="icon">
+                  <i class="fas fa-user-plus"></i>
+                </span>
+                <span>サインアップ</span>
+              </router-link>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
@@ -27,6 +61,30 @@
     </main>
   </div>
 </template>
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      user: null
+    };
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.user = user;
+    });
+  },
+  methods: {
+    async signOut() {
+      await firebase.auth().signOut();
+      // サインアウト後にトップページに遷移する
+      this.$router.push({ name: "home" });
+    }
+  }
+};
+</script>
 
 <style>
 </style>
