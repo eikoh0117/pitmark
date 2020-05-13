2507ee35222d72c05ba7025ca0d33f34f30119bd
Hidemitsu Aoki <olympos0117@gmail.com>
2020-05-13 19:35:08 +0900

    Make icons and header components

--- app/pitmark/src/views/SignUp.vue
+++ app/pitmark/src/views/SignUp.vue
@@ -1,28 +1,18 @@
 <template>
   <div>
-    <section class="hero is-primary">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">サインアップ</h1>
-        </div>
-      </div>
-    </section>
+    <pm-page-title title="サインアップ"></pm-page-title>
     <section class="section">
       <form class="container">
         <div class="field">
           <p class="control has-icons-left">
             <input class="input" type="email" placeholder="メールアドレス" v-model="email" />
-            <span class="icon is-small is-left">
-              <i class="fas fa-envelope"></i>
-            </span>
+            <pm-icon class="is-small is-left" name="envelop"></pm-icon>
           </p>
         </div>
         <div class="field">
           <p class="control has-icons-left">
             <input class="input" type="password" placeholder="パスワード" v-model="password" />
-            <span class="icon is-small is-left">
-              <i class="fas fa-lock"></i>
-            </span>
+            <pm-icon class="is-small is-left" name="lock"></pm-icon>
           </p>
         </div>
         <div class="field">
@@ -38,8 +28,12 @@
 <script>
 // import firebase from "firebase";
 import { authService } from "@/services/AuthService";
+import pmPageTitle from "@/components/PageTitle";
+import pmIcon from "@/components/Icon";
+
 export default {
   name: "sign_up",
+  components: { pmPageTitle, pmIcon },
   data() {
     return {
       email: null,
